1e7f49d1482612e6d2603703830200f497222913
jaytailor45 <tailorj64@gmail.com>
2019-05-06 13:30:23 +0530

    Project setup and created reusable headerComponent

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import Header from './components/header'
+
+class Root extends Component {
+    render() {
+        return <Header headerText={'Albums!'}/>
+    }
+}
+
+export default Root
