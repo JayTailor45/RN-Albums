c3fbb5f119077559586ee2ec30a2bbfec81cefa5
jaytailor45 <tailorj64@gmail.com>
2019-05-07 15:38:11 +0530

    Finished Project

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import React, {Component} from "react";
 import Header from './components/header'
-
+import {View} from 'react-native'
+import AlbumList from "./components/AlbumList";
 class Root extends Component {
     render() {
-        return <Header headerText={'Albums!'}/>
+        return (
+            <View style={{flex: 1}}>
+                <Header headerText={'Albums!'} />
+                <AlbumList/>
+            </View>
+        )
     }
 }
 
